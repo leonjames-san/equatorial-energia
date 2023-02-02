6fee033b2635da6eeec9c250acb8ed8df5d72bee
elizandrodantas <dantaspm@icloud.com>
2023-02-02 17:18:12 -0300

    feat: added login option with cnpj

--- src/client.ts
+++ src/client.ts
@@ -4,12 +4,12 @@ import formData from 'form-data';
 import { randomUUID } from 'node:crypto';
 import { cpf } from 'cpf-cnpj-validator';
 
-import { apiSettings, formatingBirhtDay, formatingCpf, isBirhtDay, isObject, isString, states } from './util';
+import { apiSettings, formatingBirhtDay, formatingDocument, isBirhtDay, isObject, isString, states } from './util';
 import { ClientError, ErrorRequestParse } from './error';
 import { AccessToken, InvoiceList, InstallationDetails, InvoiceHistory, InvoiceOpen } from './structure';
 
 type iStateAccept = "PI" | "MA" | "PA" | "AL";
-type iLoginType = "birthday" | "document" | "monther-name";
+type iLoginType = "birthday" | "document" | "cnpj" | "monther-name";
 
 type iOptionsClient = {
     auth?: {
@@ -21,19 +21,19 @@ type iOptionsClient = {
 }
 
 export class Client {
-    private username = "";
-    private password = "";
-    private options: iOptionsClient = {};
+    private username: string;
+    private password: string;
+    private options: iOptionsClient;
 
-    public token?: string;
+    public token: string;
     public _apiConfig;
 
     constructor(options: iOptionsClient)
     constructor(username: string, password: string, options: iOptionsClient)
     constructor(username: any, password?: any, options?: any){
         if(isObject(username)){
-            this.username = username?.auth?.username;
-            this.password = username?.auth?.password;
+            this.username = username.auth?.username;
+            this.password = username.auth?.password;
             this.options = username;
         } else if(isString(username)){
             this.username = username;
@@ -56,6 +56,7 @@ export class Client {
 
     /**
      * authenticate user with api
+     * @private
      */
 
     private async login(username: string, password: string, state: iLoginType): Promise<AccessToken>
@@ -67,7 +68,7 @@ export class Client {
             password = this.password;
         }
 
-        if(!this.username || !this.password)
+        if(!username || !password)
             throw new ClientError("username and password required for login");
 
         step = step ?? this.options?.stepLogin;
@@ -77,10 +78,7 @@ export class Client {
 
         let typing;
 
-        if(!cpf.isValid(username))
-            throw new ClientError("username invalid");
-
-        username = formatingCpf(username);
+        username = formatingDocument(username);
 
         if(step == "birthday"){
             if(!isBirhtDay(password))
@@ -96,8 +94,12 @@ export class Client {
                 password = password.substring(0, 3);
 
             typing = "2";
-        }else {
+        }else if(step === "monther-name"){
             typing = "3";
+        }else if(step === "cnpj"){
+            typing = "4";
+        }else{
+            throw new ClientError("login form not found");
         }
 
         try{
@@ -126,9 +128,9 @@ export class Client {
      * login with cpf and date of birth
      */
 
-    async loginWithBirhtday(username?: string, password?: string){
+    async loginWithBirhtday(cpf?: string, brithday?: string){
         try{
-            return await this.login(username ?? this.username, password ?? this.password, "birthday");
+            return await this.login(cpf ?? this.username, brithday ?? this.password, "birthday");
         }catch(err){
             if(err instanceof ClientError)
                 throw new ClientError(`[loginWithBirthday]: ${err.message}`);
@@ -138,15 +140,30 @@ export class Client {
     }
 
     /**
-     * login with cpf and document
+     * login with cpf and document [RG]
      */
 
-    async loginWithDocument(username?: string, password?: string){
+    async loginWithDocument(cpf?: string, document?: string){
         try{
-            return await this.login(username ?? this.username, password ?? this.password, "document");
+            return await this.login(cpf || this.username, document || this.password, "document");
         }catch(err){
             if(err instanceof ClientError)
-                throw new ClientError(`[loginWithBirthday]: ${err.message}`);
+                throw new ClientError(`[loginWithDocument]: ${err.message}`);
+
+            throw new ClientError("unknown error");
+        }
+    }
+
+    /**
+     * login with cnpj
+     */
+
+    async loginWithCnpj(cnpj?: string, email?: string){
+        try{
+            return this.login(cnpj || this.username, email || this.password, "cnpj");
+        }catch(err){
+            if(err instanceof ClientError)
+                throw new ClientError(`[loginWithCpnj]: ${err.message}`);
 
             throw new ClientError("unknown error");
         }
@@ -156,12 +173,12 @@ export class Client {
      * login with cpf and mother name
      */
 
-    async loginWithMontherName(username?: string, password?: string){
+    async loginWithMontherName(cpf?: string, mother?: string){
         try{
-            return await this.login(username ?? this.username, password ?? this.password, "monther-name");
+            return await this.login(cpf ?? this.username, mother ?? this.password, "monther-name");
         }catch(err){
             if(err instanceof ClientError)
-                throw new ClientError(`[loginWithBirthday]: ${err.message}`);
+                throw new ClientError(`[loginWithMother]: ${err.message}`);
 
             throw new ClientError("unknown error");
         }
@@ -259,6 +276,11 @@ export class Client {
             throw new ClientError(new ErrorRequestParse(err).getMessage());
         }
     }
+
+    /**!
+     * return token from instance or param
+     * @private
+     */
 
     private resolveTokenAuthorizationOptional(token?: string){
         return token ? token : this.token;
